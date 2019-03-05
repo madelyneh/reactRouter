21495dc03b58b6cc688e1c847e9e00f09b1a9e18
Madi <madelyn.e.helton@gmail.com>
2019-03-04 21:14:48 -0600

    first commit

---
+++ client/build/precache-manifest.7f86ed03ba8e66e0a78cf82ca7510756.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "26d94fa5c82aa8b16f97",
+    "url": "/static/css/main.a9967cb9.chunk.css"
+  },
+  {
+    "revision": "26d94fa5c82aa8b16f97",
+    "url": "/static/js/main.26d94fa5.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "8fdc6d53c764e3ee62ca",
+    "url": "/static/js/2.8fdc6d53.chunk.js"
+  },
+  {
+    "revision": "1ac251ed4293c4c6ad76891bc9d1e517",
+    "url": "/index.html"
+  }
+];
